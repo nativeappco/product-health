4d3f7795415c04ea3f6babd7e361bfb4d3a8e941
nativeappco <support@nativeappco.com>
2026-03-11 17:55:36 +1100

    Update server.js
    add CSP

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// Set permissive CSP so the frontend can make API calls
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com; font-src https://fonts.gstatic.com; connect-src 'self'; img-src 'self' data: https:;");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Proxy endpoint for Shopify Admin API
